2af4812246cf3c567f44c847db0539c065654629
buuoltwo <bullnick20@gmail.com>
2024-12-09 14:03:33 +0800

    创建多态的父类和子类

--- src/main/java/com/github/hcsp/polymorphism/Square.java
+++ src/main/java/com/github/hcsp/polymorphism/Square.java
@@ -1,10 +1,11 @@
 package com.github.hcsp.polymorphism;
 
-public class Square {
+public class Square extends Rectangle{
     // 正方形的边长
     private double sideLength;
 
     public Square(double sideLength) {
+        super(sideLength,sideLength);
         this.sideLength = sideLength;
     }
 }
